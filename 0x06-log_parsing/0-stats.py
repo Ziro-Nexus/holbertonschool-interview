5df604d134f34e1b57113eb4b59a87585f35a37f
Andrés Camilo Tobón Mejía <toboncamilo40@gmail.com>
2021-03-11 15:12:10 -0500

    add

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,28 +2,31 @@
 '''doc.'''
 import sys
 
+
+def get_match(line):
+    '''doc.'''
+    try:
+        line = line[:-1]
+        words = line.split(" ")
+        size[0] += int(words[-1])
+        code = int(words[-2])
+        if code in logs:
+            logs[code] += 1
+    except:
+        pass
+
+
+def p_stats():
+    '''doc'''
+    print("File size: {}".format(size[0]))
+    for k in sorted(logs.keys()):
+        if logs[k]:
+            print("{}: {}".format(k, logs[k]))
+
+
 if __name__ == "__main__":
     size = [0]
     logs = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-    def get_match(line):
-        '''doc.'''
-        try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in logs:
-                logs[code] += 1
-        except:
-            pass
-
-    def p_stats():
-        '''doc'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(logs.keys()):
-            if logs[k]:
-                print("{}: {}".format(k, logs[k]))
     i = 1
     try:
         for line in sys.stdin:

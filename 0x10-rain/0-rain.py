9918447f6fa821836b67c22a55d97e6730930585
Camilo T <toboncamilo33@gmail.com>
2021-05-27 22:23:46 -0500

    add

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,26 @@
+
+#!/usr/bin/python3
+"""
+0. Rain
+"""
+
+
+def rain(walls):
+    """
+    doc
+    """
+    length = len(walls)
+    res = 0
+
+    for i in range(1, length - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        right = walls[i]
+
+        for j in range(i + 1, length):
+            right = max(right, walls[j])
+
+        res = res + (min(left, right) - walls[i])
+    return res

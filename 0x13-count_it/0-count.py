cf91a55ac2c35f877b9fec3316fff7f12bcbf023
Camilo T <toboncamilo33@gmail.com>
2021-06-17 22:03:35 -0500

    add

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,43 +1,54 @@
 #!/usr/bin/python3
-"""Count it problem
+"""doc
 """
+
 import requests
+import sys
 
 
-def recurse(subreddit, word_list, hot_titles, after=""):
-    """doc
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                 after)
-    res = requests.get(url,
-                       headers={'User-agent': 'product'},
-                       allow_redirects=False)
+def count_words(subreddit, word_list, kw_cont={}, next_pg=None, reap_kw={}):
+    """all hot posts by keyword"""
+    headers = {"User-Agent": "nildiert"}
 
-    if res.status_code != 200:
-        return None
-    if after is None:
-        return hot_titles
+    if next_pg:
+        subr = requests.get('https://reddit.com/r/' + subreddit +
+                            '/hot.json?after=' + next_pg, headers=headers)
+    else:
+        subr = requests.get('https://reddit.com/r/' + subreddit +
+                            '/hot.json', headers=headers)
 
-    for i in res.json().get('data').get('children'):
-        title_s = i.get('data').get('title').split()
-        for word in set(word_list):
-            if word.lower() in [x.lower() for x in title_s]:
-                if hot_titles.get(word):
-                    hot_titles[word] += 1
-                else:
-                    hot_titles[word] = 1
+    if subr.status_code == 404:
+        return
 
-    after = res.json().get('data').get('after')
-    recurse(subreddit, word_list, hot_titles, after)
-    return hot_titles
+    if kw_cont == {}:
+        for word in word_list:
+            kw_cont[word] = 0
+            reap_kw[word] = word_list.count(word)
 
+    subr_dict = subr.json()
+    subr_data = subr_dict['data']
+    next_pg = subr_data['after']
+    subr_posts = subr_data['children']
 
-def count_words(subreddit, word_list):
-    """doc
-    """
-    hot_titles = recurse(subreddit, word_list, {})
-    if hot_titles:
-        for k, v in sorted(hot_titles.items(), key=lambda val: val[1],
-                           reverse=True):
-            if v != 0:
-                print('{}: {}'.format(k, v))
+    for post in subr_posts:
+        post_data = post['data']
+        post_title = post_data['title']
+        title_words = post_title.split()
+        for w in title_words:
+            for key in kw_cont:
+                if w.lower() == key.lower():
+                    kw_cont[key] += 1
+
+    if next_pg:
+        count_words(subreddit, word_list, kw_cont, next_pg, reap_kw)
+
+    else:
+        for key, val in reap_kw.items():
+            if val > 1:
+                kw_cont[key] *= val
+
+        sorted_abc = sorted(kw_cont.items(), key=lambda x: x[0])
+        sorted_res = sorted(sorted_abc, key=lambda x: (-x[1], x[0]))
+        for res in sorted_res:
+            if res[1] > 0:
+                print('{}: {}'.format(res[0], res[1]))

8015d6b07bdbc6b12a69cbc3727cb3262b19eee4
Camilo T <toboncamilo33@gmail.com>
2021-07-22 22:55:13 -0500

    add

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -1,10 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void split(int *array, int *arr, int start, int end);
+void merge(int *array, int *arr, int start, int middle, int end);
+void merge_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
